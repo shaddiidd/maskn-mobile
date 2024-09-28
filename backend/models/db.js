9ba1df2154b35609bcad7265b8654b952e724859
AnasMBajawi <anas.mohammad.bajawi@gmail.com>
2024-09-28 22:50:20 +0300

    database connetion and service resloved

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,11 +1,15 @@
-const { Pool } = require("pg");
-
+const { Pool } = require('pg');
 const pool = new Pool({
-    connectionString : process.env.DB_URL, 
-    ssl: {
-      rejectUnauthorized: false, // Disable SSL certificate verification
-    },
+  user: 'maskn_db_owner',
+  host: 'ep-late-dew-a2xnq2r1.eu-central-1.aws.neon.tech',
+  database: 'maskn_db',
+  password: 'VFoMb3P5QlSO',
+  port: 5432, // Default PostgreSQL port
+  ssl: {
+    rejectUnauthorized: false // Ignore SSL certificate errors
+  }
 });
+
 
 pool
   .connect()
