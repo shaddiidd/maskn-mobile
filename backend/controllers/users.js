094d2c441dddab119f3776fbd12a4f7f3d842449
AnasMBajawi <anas.mohammad.bajawi@gmail.com>
2024-09-29 13:29:52 +0300

    deleting node modules and adding gitignore file

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,1 +1,11 @@
-const pool = require("../models/db")
+const pool = require("../models/db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const sginUp = (req, res) => {
+  const {
+    
+  } = req.body;
+};
+
+module.exports = { sginUp };
