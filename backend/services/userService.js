46ede11d86bea7141d1d5c18f30e838a553e2d33
AnasMBajawi <anas.mohammad.bajawi@gmail.com>
2024-12-19 08:51:09 +0300

    adding get user by user id service APIs

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -6,71 +6,70 @@ const AppError = require("../utils/AppError");
 
 const createUser = async (userData, files) => {
   try {
-      // Destructure userData fields
-      const {
-          username,
-          first_name,
-          last_name,
-          national_number,
-          date_of_birth,
-          password,
-          nationality,
-          email,
-          phone_number,
-      } = userData;
+    // Destructure userData fields
+    const {
+      username,
+      first_name,
+      last_name,
+      national_number,
+      date_of_birth,
+      password,
+      nationality,
+      email,
+      phone_number,
+    } = userData;
 
-      // Hash the password
-      const hashedPassword = await bcrypt.hash(password, 10);
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-      // Default values
-      const role = 1;
-      const defaultRating = 5;
+    // Default values
+    const role = 1;
+    const defaultRating = 5;
 
-      // Handle profile photo
-      const photoUrl = files.map((file) => file.path);
+    // Handle profile photo
+    const photoUrl = files.map((file) => file.path);
 
-      // Create the user in the database
-      const newUser = await User.create({
-          username,
-          first_name,
-          last_name,
-          national_number,
-          date_of_birth,
-          password: hashedPassword,
-          nationality,
-          rating: defaultRating,
-          email,
-          role_id: role,
-          phone_number,
-          profile_photo: photoUrl,
-      });
+    // Create the user in the database
+    const newUser = await User.create({
+      username,
+      first_name,
+      last_name,
+      national_number,
+      date_of_birth,
+      password: hashedPassword,
+      nationality,
+      rating: defaultRating,
+      email,
+      role_id: role,
+      phone_number,
+      profile_photo: photoUrl,
+    });
 
-      // Create a JWT payload
-      const payload = {
-          userId: newUser.user_id,
-          country: newUser.nationality,
-          userName: newUser.username,
-          role: newUser.role_id,
-          firstName: newUser.first_name,
-          lastName: newUser.last_name,
-          profile_photo: newUser.profile_photo,
-      };
+    // Create a JWT payload
+    const payload = {
+      userId: newUser.user_id,
+      country: newUser.nationality,
+      userName: newUser.username,
+      role: newUser.role_id,
+      firstName: newUser.first_name,
+      lastName: newUser.last_name,
+      profile_photo: newUser.profile_photo,
+    };
 
-      // Generate the token
-      const options = { expiresIn: "1d" };
-      const secret = process.env.SECRET;
-      const token = jwt.sign(payload, secret, options);
+    // Generate the token
+    const options = { expiresIn: "1d" };
+    const secret = process.env.SECRET;
+    const token = jwt.sign(payload, secret, options);
 
-      // Return the created user and token
-      return {
-          user: newUser,
-          token,
-      };
+    // Return the created user and token
+    return {
+      user: newUser,
+      token,
+    };
   } catch (error) {
-      throw error; // Let the controller handle errors
+    throw error; // Let the controller handle errors
   }
 };
-
 
 const findAllUsers = async () => {
   try {
@@ -81,13 +80,21 @@ const findAllUsers = async () => {
   }
 };
 
+const findUserByUserId = async (userId) => {
+  try {
+    const user = await User.findByPk(userId);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
 
 const loginUser = async (credentials) => {
   let { email, national_number, password } = credentials;
 
   // Ensure email is always lowercase
   if (email) {
-      email = email.toLowerCase();
+    email = email.toLowerCase();
   }
 
   // Determine whether the input contains email or national number
@@ -95,29 +102,29 @@ const loginUser = async (credentials) => {
 
   // Find user by email or national number
   const user = await User.findOne({
-      where: isEmail ? { email } : { national_number },
+    where: isEmail ? { email } : { national_number },
   });
 
   // Check if user exists
   if (!user) {
-      throw new AppError("User not found", 401);
+    throw new AppError("User not found", 401);
   }
 
   // Compare provided password with stored hashed password
   const isPasswordMatch = await bcrypt.compare(password, user.password);
   if (!isPasswordMatch) {
-      throw new AppError("Password or email is incorrect", 401);
+    throw new AppError("Password or email is incorrect", 401);
   }
 
   // Generate JWT token
   const payload = {
-      userId: user.user_id,
-      country: user.nationality,
-      userName: user.username,
-      role: user.role_id,
-      firstName: user.first_name,
-      lastName: user.last_name,
-      profile_photo: user.profile_photo,
+    userId: user.user_id,
+    country: user.nationality,
+    userName: user.username,
+    role: user.role_id,
+    firstName: user.first_name,
+    lastName: user.last_name,
+    profile_photo: user.profile_photo,
   };
   const options = { expiresIn: "1d" };
   const secret = process.env.SECRET;
@@ -131,50 +138,51 @@ const loginUser = async (credentials) => {
 const acceptOwnerRequestService = async (requestId, role) => {
   // Check if the role is authorized
   if (role !== 3) {
-      throw new AppError("Unauthorized: Only admins can approve requests", 403);
+    throw new AppError("Unauthorized: Only admins can approve requests", 403);
   }
 
   // Find the rental request
   const request = await OwnersRentalRequest.findByPk(requestId);
 
   if (!request) {
-      throw new AppError("Request not found: Failed to change the role", 404);
+    throw new AppError("Request not found: Failed to change the role", 404);
   }
 
   try {
-      // Update request state to 'approved'
-      await OwnersRentalRequest.update(
-          { request_state: "approved" },
-          { where: { request_id: requestId } }
-      );
+    // Update request state to 'approved'
+    await OwnersRentalRequest.update(
+      { request_state: "approved" },
+      { where: { request_id: requestId } }
+    );
 
-      // Update the user's role to '2' (Owner)
-      await User.update(
-          { role_id: 2 },
-          { where: { user_id: request.user_id } }
-      );
+    // Update the user's role to '2' (Owner)
+    await User.update({ role_id: 2 }, { where: { user_id: request.user_id } });
 
-      // Return success response
-      return {
-          userId: request.user_id,
-          message: `The role for user ${request.user_id} is changed and request is approved`,
-      };
+    // Return success response
+    return {
+      userId: request.user_id,
+      message: `The role for user ${request.user_id} is changed and request is approved`,
+    };
   } catch (error) {
-      throw new AppError("Failed to approve the request", 500, { details: error.message });
+    throw new AppError("Failed to approve the request", 500, {
+      details: error.message,
+    });
   }
 };
-
 
 const RequestToBecomeRenterService = async (userId) => {
   // Find user by ID
   const user = await User.findByPk(userId);
 
-  //Already has a request 
-  const requestExists = await OwnersRentalRequest.findOne({where:{user_id : userId}})
-  
-  if (requestExists) throw new AppError("Users cant request more than once ", 403)
+  //Already has a request
+  const requestExists = await OwnersRentalRequest.findOne({
+    where: { user_id: userId },
+  });
+
+  if (requestExists)
+    throw new AppError("Users cant request more than once ", 403);
   if (!user) {
-      throw new AppError("User not found", 404);
+    throw new AppError("User not found", 404);
   }
 
   // Create a new request
@@ -185,17 +193,19 @@ const RequestToBecomeRenterService = async (userId) => {
 
 const getAllOwnersRequestsService = async () => {
   try {
-      // Fetch all owner rental requests
-      const requests = await OwnersRentalRequest.findAll();
+    // Fetch all owner rental requests
+    const requests = await OwnersRentalRequest.findAll();
 
-      // Check if no requests are found
-      if (!requests || requests.length === 0) {
-          throw new AppError("No owner requests found", 404);
-      }
+    // Check if no requests are found
+    if (!requests || requests.length === 0) {
+      throw new AppError("No owner requests found", 404);
+    }
 
-      return requests;
+    return requests;
   } catch (error) {
-      throw new AppError("Unable to load requests", 500, { details: error.message });
+    throw new AppError("Unable to load requests", 500, {
+      details: error.message,
+    });
   }
 };
 
@@ -204,17 +214,17 @@ const refreshToken = async (userId) => {
   const user = await User.findByPk(userId);
 
   if (!user) {
-      throw new AppError("User not found", 404);
+    throw new AppError("User not found", 404);
   }
 
   // Generate a new token
   const payload = {
-      userId: user.user_id,
-      country: user.nationality,
-      userName: user.username,
-      role: user.role_id,
-      firstName: user.first_name,
-      lastName: user.last_name,
+    userId: user.user_id,
+    country: user.nationality,
+    userName: user.username,
+    role: user.role_id,
+    firstName: user.first_name,
+    lastName: user.last_name,
   };
   const options = { expiresIn: "1d" };
   const secret = process.env.SECRET;
@@ -232,4 +242,5 @@ module.exports = {
   RequestToBecomeRenterService,
   getAllOwnersRequestsService,
   refreshToken,
+  findUserByUserId
 };
